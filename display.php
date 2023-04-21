aa52b9a881f8d0071c611e5c2744a43972b86951
TleAim <udomsak25@hotmail.com>
2023-04-21 14:46:10 +0700

    new commit

--- display.php
+++ display.php
@@ -1,20 +1,5 @@
 <?php
-function menu_navi($pname){
-    echo "
-    <nav class=\"navbar navbar-expand-md bg-dark navbar-dark\">
-    <a class=\"navbar-brand\" href=\"./index.php\">".$pname."</a>
-    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapsibleNavbar\">
-      <span class=\"navbar-toggler-icon\"></span>
-    </button>
-    <div class=\"collapse navbar-collapse\" id=\"collapsibleNavbar\">
-      <ul class=\"navbar-nav\">
-        <li class=\"nav-item\"><a class=\"nav-link\" href=\"index.php\">หน้าหลัก</a></li>  
-        <li class=\"nav-item\"><a class=\"nav-link\" href=\"#contact\">ติดต่อเรา</a></li>    
-      </ul>
-    </div>  
-  </nav>
-    ";
-  }
+
 
 function is_mobile() {
     $user_agent = $_SERVER['HTTP_USER_AGENT'];
